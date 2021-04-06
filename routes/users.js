597e17cb668addba2716d4e0fd0e87fdebd6e9ff
Colton Eckenrode <cje5311@psu.edu>
2021-04-06 16:58:48 -0400

    Users changes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.query);
+  console.log(req.body);
+
+  let data = {
+    hello:"hello",
+    world:"world",
+    query: req.query
+  };
+
+  res.send(data);
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('respond with a test resource');
+});
+
 module.exports = router;
