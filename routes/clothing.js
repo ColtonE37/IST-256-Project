45cb23dd673f7e7769be2711c59a0af9331c0ab2
Colton Eckenrode <cje5311@psu.edu>
2021-04-09 19:03:46 -0400

    test

--- routes/clothing.js
+++ routes/clothing.js
@@ -1,6 +1,6 @@
-// var axios = require('axios').default; //importing axios to make a rquest
-// var express = require('express');
-// var router = express.Router();
+var axios = require('axios').default; //importing axios to make a rquest
+var express = require('express');
+var router = express.Router();
 
 // //Refer to README.txt about issues we've encountered and our understanding of API's
 // router.get('/', function (req, res, next) {
