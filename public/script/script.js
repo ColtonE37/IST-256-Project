bae0125068d659c1b3c58c9292b718f2f7f09372
Colton Eckenrode <cje5311@psu.edu>
2021-04-26 19:02:08 -0400

    Commit

--- public/script/script.js
+++ public/script/script.js
@@ -23,6 +23,7 @@ function filterFunction() {
 }
 
 $(document).ready(function () {
+  doAjaxRequest();
   console.log("Ready!");
   makeRequest('GET', '/currentCart', (response) => {
     console.log("Current Cart: ", response); //response is cart object
@@ -95,6 +96,29 @@ $(document).ready(function () {
       })
     }
   });
+  // makeRequest('GET', '/getCartProducts', (response) => {
+  //   for (var i = 0; i < response.length; i++) {
+  //     $('.api-data-cart')
+  //       .append('<h3>' + response[i].title + '</h3>')
+  //       .append('<span>' + '$' + response[i].price + '</span> &nbsp')
+  //   }
+  // }).then((response) => {
+  //     makeRequest('PUT', '/getCartProducts', (cartProducts) => {
+  //       console.log("Get products called.", cartProducts);
+  //     }, error => {
+  //       console.error("Error occurred.", error);
+  //     })
+  // });
+
+  function doAjaxRequest() {
+    $.ajax({
+        type: "GET",
+        url: "/getCartProducts"
+    }).done(function (response) {
+        console.log(response);
+        displayData(response);
+    });
+}
 
   function makeRequest(method, url, successFn, errorFn, data) {
     return $.ajax({
@@ -150,4 +174,16 @@ function sendContact() {
 //checkout page
 function placeOrder() {
   alert(firstName.value + " " + "Your order has been placed! Thanks for shopping with us!")
+}
+
+function deleteCart() {
+  alert("The cart has been deleted!");
+  window.location.reload();
+}
+
+//Cart page products
+function displayData(data) {
+  let content = $("#content");
+  content.append($(`<h3>${data.title}</h3>`));
+  content.append($(`<h3>${data.price}</h3>`));
 }
